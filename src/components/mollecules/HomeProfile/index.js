b251164b185f74b794262e9842bbc6a23c070fdc
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2020-11-23 10:21:30 +0700

    get local storrage to display

--- src/components/mollecules/HomeProfile/index.js
+++ src/components/mollecules/HomeProfile/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
-import {DummyUser} from '../../../assets';
+import {DummyUser, ILUserFotoNull} from '../../../assets';
 import {fonts, colors} from '../../../utils';
+import {getData} from '../../../utils/LocalStorage';
 
 const HomeProfile = ({onPress}) => {
+  useEffect(() => {
+    getData('user').then((res) => {
+      console.log(res);
+      //foto berupa string kita rubah ke sebuah objek
+      const data = res;
+      data.photo = {uri: res.photo};
+
+      setprofile(res);
+    });
+  }, []);
+  const [profile, setprofile] = useState({
+    photo: ILUserFotoNull,
+    fullName: '',
+    profession: '',
+  });
   return (
     <TouchableOpacity onPress={onPress} style={styles.container}>
-      <Image source={DummyUser} style={styles.avatar} />
+      <Image source={profile.photo} style={styles.avatar} />
       <View>
-        <Text style={styles.name}>Shayna Melinda</Text>
-        <Text style={styles.profesi}>Product Manager</Text>
+        <Text style={styles.name}>{profile.fullName}</Text>
+        <Text style={styles.profesi}>{profile.profession}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -27,15 +43,18 @@ const styles = StyleSheet.create({
     width: 46,
     height: 46,
     marginRight: 12,
+    borderRadius: 46 / 2,
   },
   name: {
     fontSize: 16,
     color: colors.text.primary,
     fontFamily: fonts.primary[600],
+    textTransform: 'capitalize',
   },
   profesi: {
     fontSize: 12,
     fontFamily: fonts.primary[400],
     color: colors.text.secondary,
+    textTransform: 'capitalize',
   },
 });
